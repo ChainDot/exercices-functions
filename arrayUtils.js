d150d56e375d2485135bea226794fe388f5f4700
ChainDot <crypto.dot@protonmail.com>
2021-03-03 19:56:09 +0100

    update

--- arrayUtils.js
+++ arrayUtils.js
@@ -10,10 +10,10 @@ const sortAscend = (array) => {
 
 } 
  const makeUnique = (array) => {
-  let uniqueArray = []
+  let uniqueArray = [] // new empty array
   for( let i = 0; i< array.length; i++){
     if(uniqueArray.indexOf(array[i]) === -1) { // return -1 if the search value can't be found
-      uniqueArray.push(array[i]);
+      uniqueArray.push(array[i]); // push unique number into new array
     }
   }
   return uniqueArray
@@ -24,9 +24,9 @@ const sortAscend = (array) => {
 const array = [99, 100, 101, 1]
 const array2 = [1, 2, 1, 3, 2, 4, 5, 7, 5, 1]
 
-maxNb = biggest(array)
-orderNb = sortAscend(array)
-orderUniqueNb = makeUnique(array2)
+const maxNb = biggest(array)
+const orderNb = sortAscend(array)
+const orderUniqueNb = makeUnique(array2)
 
 console.log(maxNb)
 console.log(orderNb)
